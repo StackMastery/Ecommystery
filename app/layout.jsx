2d1a3eb3639cb7901cd60d766eabeea19d2283e0
= <=>
2025-04-25 16:32:10 +0600

    Website Zoom in zoom out turned off

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,7 +2,7 @@ import { Instrument_Serif, Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import Header from "./components/widget/Header";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import Head from "next/head"; // Importing Head for meta tag
+import { Viewport } from "next";
 
 const plusJakarta = Plus_Jakarta_Sans({
   subsets: ["latin"],
@@ -20,20 +20,21 @@ export const metadata = {
   },
 };
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  // Also supported but less commonly used
+  // interactiveWidget: 'resizes-visual',
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        data-new-gr-c-s-check-loaded="14.1232.0"
-        data-gr-ext-installed=""
         className={`bg-bodyBg overflow-x-hidden text-white [&::-webkit-scrollbar]:w-0 ${plusJakarta.className} antialiased`}
       >
-        <Head>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-          />
-        </Head>
         <TooltipProvider>
           <Header />
           {children}
--- components/ui/button.jsx
+++ components/ui/button.jsx
@@ -14,13 +14,13 @@ function Button({
     <button
       data-slot="button"
       className={cn(
-        `relative px-7 outline-0 flex items-center transition-all gap-2 group h-[40px] uppercase border border-transparent hover:border-pM/20 cursor-pointer rounded-full text-sm bg-transparent  overflow-hidden`,
+        `relative px-7 outline-0 flex items-center transition-all gap-2 group h-[40px] uppercase border border-transparent hover:border-pM/20 focus:border-pM/20 cursor-pointer rounded-full text-sm bg-transparent overflow-hidden`,
         className
       )}
       {...props}
     >
       {/* SVG Background */}
-      <SVGGredient className="absolute transition-all blur-md group-hover:scale-150 group-hover:rotate-45 inset-0 z-0" />
+      <SVGGredient className="absolute transition-all blur-md group-hover:scale-150 group-hover:rotate-45 group-focus:scale-150 group-focus:rotate-45 inset-0 z-0" />
 
       {/* Button Content */}
       <span className="z-10 flex items-center gap-1">{children}</span>
