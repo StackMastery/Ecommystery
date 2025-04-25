cd8f236e662052138fc35aa1b5ca0c32fbfec3dc
= <=>
2025-04-25 16:22:53 +0600

    Website Zoom in zoom out turned off

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import { Instrument_Serif, Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import Header from "./components/widget/Header";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import Head from "next/head"; // Importing Head for meta tag
 
 const plusJakarta = Plus_Jakarta_Sans({
   subsets: ["latin"],
@@ -27,6 +28,12 @@ export default function RootLayout({ children }) {
         data-gr-ext-installed=""
         className={`bg-bodyBg overflow-x-hidden text-white [&::-webkit-scrollbar]:w-0 ${plusJakarta.className} antialiased`}
       >
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+          />
+        </Head>
         <TooltipProvider>
           <Header />
           {children}
